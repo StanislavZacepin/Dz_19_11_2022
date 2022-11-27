1191c08be90db870ae1abe4958338f4a1a41a47a
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-11-27 22:26:08 +0300

    Рефакторинг

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,11 +7,11 @@ import  model.Record;
 public class Main {
     public static void main(String[] args) {
 
-//        People scan = ScannerText.scannerText();
-//        System.out.println(scan.getLastName() + " " + scan.getFirstName() + " " + scan.getPatronymic() + " "
-//                + scan.getBirthdate() + " " + scan.getPhoneNumber() + " " + scan.getGender());
-//        Record.findDirectory();
-//        Record.record(scan);
+       People scan = ScannerText.scannerText();
+       System.out.println(scan.getLastName() + " " + scan.getFirstName() + " " + scan.getPatronymic() + " "
+                + scan.getBirthdate() + " " + scan.getPhoneNumber() + " " + scan.getGender());
+        Record.findDirectory();
+        Record.record(scan);
 
             // DZ2
             System.out.println(ScannerLoginAndPasword.scannerLoginAndPasword());
